1e8f5947e0026dadd8a2d8710dba0bb588fe02f4
Dylan Cole <logboarddjc@gmail.com>
2021-03-30 23:00:14 -0400

    write html and css + most of script function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /* questions are going to be stored in objects 
-example:
+example: 
 let question1 = {
     correctAnswer: "answer",
     decoy1: "decoy",
@@ -23,31 +23,100 @@ for (i = 0; i < questions.length; i++) {
     ++i;
 }
 */
+// 
+// questions created
+let question1 = {
+    question: "this is where the question 1 is to be stowed",
+    correctAnswer: "answer",
+    decoy1: "decoy",
+    decoy2: "decoy",
+    decoy3: "decoy",
+}
+
+let question2 = {
+    question: "this is where the question 2 is to be stowed",
+    correctAnswer: "answer",
+    decoy1: "decoy",
+    decoy2: "decoy",
+    decoy3: "decoy",
+}
+
+let question3 = {
+    question: "this is where the question 3 is to be stowed",
+    correctAnswer: "answer",
+    decoy1: "decoy",
+    decoy2: "decoy",
+    decoy3: "decoy",
+}
+
+let question4 = {
+    question: "this is where the question 4 is to be stowed",
+    correctAnswer: "answer",
+    decoy1: "decoy",
+    decoy2: "decoy",
+    decoy3: "decoy",
+}
+
+// array of question objects
+let questionsList = [question1, question2, question3, question4];
+
+function init() {
+
+}
+// Make variables of and create array of the initially displayed elements 
+let main = document.querySelector("main");
+let mainTitle = document.querySelector("#main-title");
+let mainPara = document.querySelector("#main-para");
+let startButton = document.querySelector("#start-button");
+let answerButton = document.querySelector(".answer-button");
+
+let initialElements = [mainTitle, mainPara, startButton];
+
+// event listener for start button
+startButton.addEventListener("click", function() {
+
+    // removes initial text
+    for(let i = 0; i < initialElements.length; i++) {
+        initialElements[i].remove();
+    }
+
+    // create next question loop until all questions have been asked
+    // put this in a function
+    let i = 0;
+    function nextQuestion() {
+        let displayQuestion = document.createElement("h1");
+        displayQuestion.textContent = questionsList[i].question;
+        main.appendChild(displayQuestion);
 
 
+        let choices = [
+            questionsList[i].correctAnswer,
+            questionsList[i].decoy1,
+            questionsList[i].decoy2,
+            questionsList[i].decoy3,
+        ].sort(function () {
+            return Math.random();
+        });
 
-H1 = document.querySelector("h1");
-startButton = document.querySelector(".button-section");
-
-let openingH = "This is a breif paragraph discribing what to do in this situation.";
-
-startButton.addEventListener("click", function(event) {
-    let element = event.target;
-
-    if(element.matches(".start-button")) {
-        let state = startButton.getAttribute("data-state");
-    
-        if (state === "hidden") {
-            element.dataset.state = "visible";
-            element.textContent = element.dataset.text;
-        } else {
-            element.dataset.state = "hidden";
-            startButton.textContent = "";
-
+        for (let x = 0; x < choices.length; x++) {
+            let answerButtons = document.createElement("button");
+            answerButtons.textContent = choices[x];
+            answerButtons.setAttribute('class', 'answer-button');
+            main.appendChild(answerButtons);
         }
-   
+        
+        i++;
+    }
+    nextQuestion();
+    answerButton.addEventListener("click", function(event) {
+        console.log(event.target.textContent);
+        if (event.target.textContent === questionsList[i].correctAnswer) {
+            console.log("wowoow!!!");
+        } else {
+            console.log("nooooaaahH!");
+        }
 
-    }    
+    });
 
 });
 
