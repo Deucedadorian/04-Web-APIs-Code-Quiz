ca52c98a2b5069c8bfdf57faf2a8ab0d40e1c58b
Dylan Cole <logboarddjc@gmail.com>
2021-03-31 19:43:12 -0400

    fix the function so that start quiz works as exspected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,122 +1,129 @@
-/* questions are going to be stored in objects 
-example: 
-let question1 = {
-    correctAnswer: "answer",
-    decoy1: "decoy",
-    decoy2: "decoy",
-    decoy3: "decoy",
-}
-let score = 0;
-let time interval = 75;
-
-when the user clicks an answer, the user's choice is compared against correctAnswer:
-
-for (i = 0; i < questions.length; i++) {
-
-    let askQuestion = function() {
-        if (event.target.textContent === correctAnswer) {
-            score += 1;    
-        } else {
-            time -= 10;
-        }
-    }
-    ++i;
-}
-*/
-// 
 // questions created
 let question1 = {
     question: "this is where the question 1 is to be stowed",
-    correctAnswer: "answer",
-    decoy1: "decoy",
-    decoy2: "decoy",
-    decoy3: "decoy",
+    correctAnswer: "answer1",
+    decoy1: "decoy1",
+    decoy2: "decoy1",
+    decoy3: "decoy1",
 }
 
 let question2 = {
     question: "this is where the question 2 is to be stowed",
-    correctAnswer: "answer",
-    decoy1: "decoy",
-    decoy2: "decoy",
-    decoy3: "decoy",
+    correctAnswer: "answer2",
+    decoy1: "decoy2",
+    decoy2: "decoy2",
+    decoy3: "decoy2",
 }
 
 let question3 = {
     question: "this is where the question 3 is to be stowed",
-    correctAnswer: "answer",
-    decoy1: "decoy",
-    decoy2: "decoy",
-    decoy3: "decoy",
+    correctAnswer: "answer3",
+    decoy1: "decoy3",
+    decoy2: "decoy3",
+    decoy3: "decoy3",
 }
 
 let question4 = {
     question: "this is where the question 4 is to be stowed",
-    correctAnswer: "answer",
-    decoy1: "decoy",
-    decoy2: "decoy",
-    decoy3: "decoy",
+    correctAnswer: "answer4",
+    decoy1: "decoy4",
+    decoy2: "decoy4",
+    decoy3: "decoy4",
 }
+
+// score and time variables
+let score = 0;
+let time = 75;
 
 // array of question objects
 let questionsList = [question1, question2, question3, question4];
 
-function init() {
-
-}
 // Make variables of and create array of the initially displayed elements 
 let main = document.querySelector("main");
 let mainTitle = document.querySelector("#main-title");
 let mainPara = document.querySelector("#main-para");
 let startButton = document.querySelector("#start-button");
-let answerButton = document.querySelector(".answer-button");
+let timerEl = document.querySelector(".timer");
 
 let initialElements = [mainTitle, mainPara, startButton];
 
+
+
+// sets index of what question is being asked to 0
+let i = 0;
+
+// create next question loop until all questions have been asked
+function nextQuestion() {
+    // create button section
+    let questionContainer = document.createElement("section");
+    main.appendChild(questionContainer);
+    questionContainer.setAttribute("class", "question");
+
+    // display question
+    let displayQuestion = document.createElement("h1");
+    displayQuestion.textContent = questionsList[i].question;
+    questionContainer.appendChild(displayQuestion);
+
+    // TODO: array of answers that are sorted randomly
+    let choices = [
+        questionsList[i].correctAnswer,
+        questionsList[i].decoy1,
+        questionsList[i].decoy2,
+        questionsList[i].decoy3,
+    ].sort(function () {
+        return Math.random();
+    });
+
+    
+
+    // creates the buttons and assigns them text
+    for (let x = 0; x < choices.length; x++) {
+        let answerButtons = document.createElement("button");
+        answerButtons.textContent = choices[x];
+        questionContainer.appendChild(answerButtons);
+    }
+
+    // add event listener for buttons 
+    questionContainer.addEventListener("click", function (event) {
+        // determines what happens when an answer is chosen
+        questionContainer.remove();
+        if (event.target.textContent === questionsList[i].correctAnswer) {
+            score++;
+        } else {
+            time = time - 10;
+        }
+        console.log("score: " + score);
+        console.log("time: " + time);
+
+
+        // if the question asked is not the last question, 
+        // the function is run again and the next question is displayed
+        if (i < questionsList.length - 1) {
+            console.log("i: " + i);
+            i++;
+            
+            nextQuestion();
+            return;
+        } else {
+            score = score * time;
+            // TODO: this will run the score function and allow the
+            // user to save their score and initials
+            
+
+            return score;
+            
+        }
+    });
+}
+
 // event listener for start button
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", function () {
 
     // removes initial text
-    for(let i = 0; i < initialElements.length; i++) {
+    for (let i = 0; i < initialElements.length; i++) {
         initialElements[i].remove();
     }
 
-    // create next question loop until all questions have been asked
-    // put this in a function
-    let i = 0;
-    function nextQuestion() {
-        let displayQuestion = document.createElement("h1");
-        displayQuestion.textContent = questionsList[i].question;
-        main.appendChild(displayQuestion);
-
-
-        let choices = [
-            questionsList[i].correctAnswer,
-            questionsList[i].decoy1,
-            questionsList[i].decoy2,
-            questionsList[i].decoy3,
-        ].sort(function () {
-            return Math.random();
-        });
-
-        for (let x = 0; x < choices.length; x++) {
-            let answerButtons = document.createElement("button");
-            answerButtons.textContent = choices[x];
-            answerButtons.setAttribute('class', 'answer-button');
-            main.appendChild(answerButtons);
-        }
-        
-        i++;
-    }
     nextQuestion();
-    answerButton.addEventListener("click", function(event) {
-        console.log(event.target.textContent);
-        if (event.target.textContent === questionsList[i].correctAnswer) {
-            console.log("wowoow!!!");
-        } else {
-            console.log("nooooaaahH!");
-        }
-
-    });
-
 });
 
